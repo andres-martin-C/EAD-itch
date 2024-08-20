49d0aa26ee111fb1747cd33ad8e51370ccffc697
andres-martin-C <172848384+andres-martin-C@users.noreply.github.com>
2024-08-20 17:38:14 -0500

    Modificacion: shadow text banner de inicio

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,25 @@ module.exports = {
     extend: {
       screens: {
         'max-400': { 'max': '400px' },
+      },
+      textShadow: {
+        'default': '3px 0px 18px #FFFFFF, -3px 3px 3px #FF7700',
+        'none': 'none'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    function ({addUtilities}) {
+      const newUtilities = {
+        '.text-shadow-default': {
+          textShadow: '3px 0px 18px #FFFFFF, -3px 3px 3px #FF7700'
+        },
+        '.text-shadow-none': {
+          textShadow: 'none'
+        }
+      }
+      addUtilities(newUtilities)
+    }
+  ],
 }
 
